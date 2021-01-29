186a85058151ce25cc532af457ba928316e35fd6
saulo <saulobach1@gmail.com>
2021-01-29 10:30:31 +0000

    Test falla cuando codifico un caracter

--- src/test/java/test/RLE_.java
+++ src/test/java/test/RLE_.java
@@ -1,14 +1,18 @@
 package test;
 
 import Model.RLE;
-import org.assertj.core.groups.Tuple;
+import Model.Tupla;
 import org.junit.Test;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class RLE_ {
     @Test
-    public void enconde_empty_string_should_return_empty_array() {
-        assertThat(RLE.encode("")).isEqualTo(new Tuple[]{});
+    public void encode_empty_string_should_return_empty_array() {
+        assertThat(RLE.encode("")).isEqualTo(new Tupla[]{});
+    }
+    @Test
+    public void encode_one_character_should_return_one_tuple() {
+        assertThat(RLE.encode("a")).isEqualTo(new Tupla[]{new Tupla('a',1)});
     }
 }
